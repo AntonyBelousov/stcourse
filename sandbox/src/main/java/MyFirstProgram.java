98ee7b897b85c8e06f439f52e20bbb58bc3635db
AntonyBelousov <anttrump@gmail.com>
2023-04-13 04:59:19 +0700

    Добавлен класс Point и метод distance

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,5 +1,13 @@
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Point p1 = new Point(3,4);
+        Point p2 = new Point(7,1);
+
+        System.out.println("Distance from function = " + distance(p1, p2));
+        System.out.println("Distance from method = " + Point.distance(p1, p2));
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p1.x-p2.x)*(p1.x- p2.x)+(p1.y-p2.y)*(p1.y- p2.y));
     }
 }
