fe7dbd3ee8aa8e4e7e484bb1dc5d872a0a016ff3
AntonyBelousov <anttrump@gmail.com>
2023-04-16 22:22:37 +0700

    Задание №3. Подключен testng и добавлены тесты для класса Point

---
+++ sandbox/src/test/java/PointTest.java
@@ -0,0 +1,33 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+
+    @Test
+    public void test1Distance() {
+        Point p1 = new Point(3,4);
+        Point p2 = new Point(7,1);
+        Assert.assertEquals(p2.distance(p1), 5);
+    }
+
+    @Test
+    public void test2Distance() {
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(0,0);
+        Assert.assertEquals(p2.distance(p1), 0);
+    }
+
+    @Test
+    public void test3Distance() {
+        Point p1 = new Point(-3.5,-4.5);
+        Point p2 = new Point(-7.5,-1.5);
+        Assert.assertEquals(p2.distance(p1), 5);
+    }
+
+    @Test
+    public void test4Distance() {
+        Point p1 = new Point(-3,-4);
+        Point p2 = new Point(3,4);
+        Assert.assertEquals(p2.distance(p1), 10);
+    }
+}
