ba39961c351b95feec1f1d08707c2f924fd7dd69
AntonyBelousov <anttrump@gmail.com>
2023-07-06 18:26:17 +0700

    Добавлены классы-хелперы, добавлены базовые тесты авторизации и регистрации

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -14,9 +14,12 @@ import java.util.Properties;
 public class ApplicationManager {
 
     private final Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
+    private RegistrationHelper registrationHelper;
+    private MailHelper mailHelper;
 
     private String browser;
+    private FtpHelper ftpHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -26,20 +29,55 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
-
-        if(browser.equals(Browser.CHROME.browserName())) {
-            wd = new ChromeDriver();
-        } else if(browser.equals(Browser.FIREFOX.browserName())) {
-            wd = new FirefoxDriver();
-        } else if(browser.equals(Browser.EDGE.browserName())) {
-            wd = new EdgeDriver();
-        }
-        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-        wd.get(properties.getProperty("baseUrl"));
     }
 
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
     }
 
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if(browser.equals(Browser.CHROME.browserName())) {
+                wd = new ChromeDriver();
+            } else if(browser.equals(Browser.FIREFOX.browserName())) {
+                wd = new FirefoxDriver();
+            } else if(browser.equals(Browser.EDGE.browserName())) {
+                wd = new EdgeDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+            wd.get(properties.getProperty("baseUrl"));
+        }
+        return wd;
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper =  new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public FtpHelper ftp() {
+        if (ftpHelper == null) {
+            ftpHelper =  new FtpHelper(this);
+        }
+        return ftpHelper;
+    }
+
+    public MailHelper mail(){
+        if (mailHelper == null) {
+            mailHelper = new MailHelper(this);
+        }
+        return mailHelper;
+    }
 }
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/BaseHelper.java
@@ -0,0 +1,65 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+public class BaseHelper {
+    protected ApplicationManager app;
+    protected WebDriver wd;
+
+    public BaseHelper(ApplicationManager app) {
+        this.app = app;
+        this.wd = app.getDriver();
+    }
+
+    protected void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        wd.findElement(locator).click();
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    protected void attach(By locator, File file) {
+        if (file != null) {
+            wd.findElement(locator).sendKeys(file.getAbsolutePath());
+        }
+    }
+
+    public boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    public void acceptAlert() {
+        if(isAlertPresent()) {
+            wd.switchTo().alert().accept();
+        }
+    }
+
+    public void checkAlertText(String alertText) {
+        assertEquals(alertText, wd.switchTo().alert().getText());
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            wd.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+
+    }
+}
